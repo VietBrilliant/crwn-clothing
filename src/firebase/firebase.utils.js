9a74b91ca51fc771b002235c2a67d911f6dbca85
Hieu Duong <hieu.duong@vietbrilliant.com>
2020-08-13 13:02:47 -0700

    Implement code that pull shop data from firestore and stores it

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,17 +1,17 @@
-import firebase from "firebase/app";
+import firebase from 'firebase/app';
 
-import "firebase/firestore";
-import "firebase/auth";
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const config = {
-  apiKey: "AIzaSyDN_5xMu_z6uDB38x7iZ6E5-MFGTgPXUuA",
-  authDomain: "crwn-clothing-db-52630.firebaseapp.com",
-  databaseURL: "https://crwn-clothing-db-52630.firebaseio.com",
-  projectId: "crwn-clothing-db-52630",
-  storageBucket: "crwn-clothing-db-52630.appspot.com",
-  messagingSenderId: "1039053516976",
-  appId: "1:1039053516976:web:8a05c4cc802e5d42a00b75",
-  measurementId: "G-30RNRLNSRN",
+  apiKey: 'AIzaSyDN_5xMu_z6uDB38x7iZ6E5-MFGTgPXUuA',
+  authDomain: 'crwn-clothing-db-52630.firebaseapp.com',
+  databaseURL: 'https://crwn-clothing-db-52630.firebaseio.com',
+  projectId: 'crwn-clothing-db-52630',
+  storageBucket: 'crwn-clothing-db-52630.appspot.com',
+  messagingSenderId: '1039053516976',
+  appId: '1:1039053516976:web:8a05c4cc802e5d42a00b75',
+  measurementId: 'G-30RNRLNSRN',
 };
 
 export const createUserProfileDocument = async (userAuth, addtionalData) => {
@@ -32,7 +32,7 @@ export const createUserProfileDocument = async (userAuth, addtionalData) => {
         ...addtionalData,
       });
     } catch (error) {
-      console.log("Error creating user ", error.message);
+      console.log('Error creating user ', error.message);
     }
   }
 
@@ -41,11 +41,45 @@ export const createUserProfileDocument = async (userAuth, addtionalData) => {
 
 firebase.initializeApp(config);
 
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+
+  objectsToAdd.forEach((obj) => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map((doc) => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items,
+    };
+  });
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
+};
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
+provider.setCustomParameters({ prompt: 'select_account' });
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,14 +1,72 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from '../../firebase/firebase.utils';
 
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
-const ShopPage = ({ match }) => (
-  <div className='shop-page'>
-    <Route exact path={`${match.path}`} component={CollectionsOverview}></Route>
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+import { updateCollections } from '../../redux/shop/shop.actions';
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
 
-export default ShopPage;
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionsPageWithSpinner = WithSpinner(CollectionPage);
+
+class ShopPage extends React.Component {
+  state = {
+    loading: true,
+  };
+
+  unsubcribleFromSnapshot = null;
+
+  componentDidMount() {
+    const { updateCollections } = this.props;
+    const collectionRef = firestore.collection('collections');
+
+    // collectionRef.onSnapshot(async (snapshot) => {
+    //   const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+    //   updateCollections(collectionMap);
+    //   this.setState({ loading: false });
+    // });
+
+    collectionRef.get().then((snapshot) => {
+      const collectionMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionMap);
+      this.setState({ loading: false });
+    });
+  }
+
+  render() {
+    const { match } = this.props;
+    const { loading } = this.state;
+
+    return (
+      <div className='shop-page'>
+        <Route
+          exact
+          path={`${match.path}`}
+          render={(props) => (
+            <CollectionsOverviewWithSpinner isLoading={loading} {...props} />
+          )}
+        ></Route>
+        <Route
+          path={`${match.path}/:collectionId`}
+          render={(props) => (
+            <CollectionsPageWithSpinner isLoading={loading} {...props} />
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  updateCollections: (collectionMap) =>
+    dispatch(updateCollections(collectionMap)),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
